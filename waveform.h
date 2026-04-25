163964955034315664daf2a1d82894674f5df039
Abdullah Alshammari <nlix906@gmail.com>
2026-04-25 10:59:30 +0100

    Define a WaveformSample struct with 8 fields to read one single row from csv file

--- waveform.h
+++ waveform.h
@@ -5,4 +5,17 @@
 #ifndef WAVEFORM_H
 #define WAVEFORM_H
 
+typedef struct
+{
+    double timestamp;
+    double phase_A;
+    double phase_B;
+    double phase_C;
+    double current;
+    double power_factor;
+    double percentage;
+    double frequency;
+
+}WaveformSample ;
 #endif //WAVEFORM_H
+
